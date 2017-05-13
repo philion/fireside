9bca9cf9344b9cbab00a0ae4f225ea49aa7f850b
Paul Philion <philion@acmerocket.com>
2017-05-13 12:42:15 -0700

    Tinkering with story structure. Can it be parsed from fairytails?

---
+++ src/main/java/com/acmerocket/fireside/TestNLP.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2016 Acme Rocket Company [acmerocket.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acmerocket.fireside;
+
+import java.util.List;
+
+//import edu.stanford.nlp.simple.Sentence;
+
+/**
+ * @author philion
+ *
+ */
+public class TestNLP {
+	
+	public static void main(String[] args) {
+		//Sentence sent = new Sentence("Lucy is in the sky with diamonds.");
+		//List<String> nerTags = sent.nerTags();  // [PERSON, O, O, O, O, O, O, O]
+		//String firstPOSTag = sent.posTag(0);   // NNP
+		
+		//System.out.println(nerTags);
+		//System.out.println(firstPOSTag);
+
+	}
+
+}
